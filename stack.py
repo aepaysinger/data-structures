618c583340495d3c8aa1b279442fe20f5fcbfb6d
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-05 14:00:43 -0500

    usinging linkedlist

--- stack.py
+++ stack.py
@@ -1,52 +1,21 @@
-from linkedlist import LinkedList, Node
+from linkedlist import LinkedList
 
 
-# class Stack:
-#     def __init__(self, values=None):
-#         self.ll = LinkedList(values)
-#         self._length = 0
-
-
-#     def push(self, val):
-#         self.ll.push(val)
-#         self._length += 1
-
-#     def pop(self):
-#         if self.ll.head:
-#             self._length -= 1
-#             return self.ll.pop()
-#         else:
-#             raise ValueError("The stack is empty.")
-
-#     def __len__(self):
-#         return self.ll._length
 class Stack:
     def __init__(self, values=None):
-        self.head = None
+        self.ll = LinkedList(values)
         self._length = 0
-        if values:
-            for value in values:
-                self.head = Node(value, self.head)
-                self._length += 1
 
-    def push(self, value):
-        self.head = Node(value, self.head)
+    def push(self, val):
+        self.ll.push(val)
         self._length += 1
 
     def pop(self):
-        old_head = self.head
-        if self._length > 0:
-            self.head = self.head.next
+        if self.ll.head:
             self._length -= 1
+            return self.ll.pop()
         else:
             raise ValueError("The stack is empty.")
-        return old_head
-
 
     def __len__(self):
-        return self._length
-
-
-
-
-  
+        return self.ll._length
--- test_stack.py
+++ test_stack.py
@@ -3,65 +3,36 @@ import pytest
 from stack import Stack
 
 
-# def test_stack():
-#     s = Stack([3, 6, 2])
-
-#     assert s.ll._length == 3
-#     assert s.ll.head.value == 2
-
-
-# def test_push():
-#     s = Stack([7, 3, "yes"])
-#     s.push("please")
-
-#     assert s.ll._length == 4
-
-
-
-# def test_pop():
-#     s = Stack([6, 2, 6, 1])
-
-#     assert s.pop() == 1
-#     assert s.ll._length == 3
-
-
-# def test_pop_empty():
-#     s = Stack([])
-
-#     with pytest.raises(ValueError) as exc_info:
-#         s.pop()
-#     assert exc_info.value.args[0] == "The stack is empty."
-
-
-# def test_len():
-#     s = Stack(["go", "fight", "win"])
-
-#     assert len(s) == 3
-
 def test_stack():
-    s = Stack([3, 6, 4])
+    s = Stack([3, 6, 2])
 
-    assert s.head.value == 4
-    assert s._length == 3
+    assert s.ll._length == 3
+    assert s.ll.head.value == 2
 
 
-def test_stack_push():
-    s = Stack([5, 3, 8])
-    s.push("number")
+def test_push():
+    s = Stack([7, 3, "yes"])
+    s.push("please")
 
-    assert s.head.value == "number"
-    assert s._length == 4
+    assert s.ll._length == 4
 
 
 def test_pop():
-    s = Stack([5, 8, 2])
-    s.pop()
+    s = Stack([6, 2, 6, 1])
 
-    assert s.head.value == 8
-    assert s._length == 2
+    assert s.pop() == 1
+    assert s.ll._length == 3
+
+
+def test_pop_empty():
+    s = Stack([])
+
+    with pytest.raises(ValueError) as exc_info:
+        s.pop()
+    assert exc_info.value.args[0] == "The stack is empty."
 
 
 def test_len():
-    s = Stack([5, 7, 2])
+    s = Stack(["go", "fight", "win"])
 
     assert len(s) == 3
