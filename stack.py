571e7f2586e176f7e4d13f47a982462d01f1cabe
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-06 07:45:01 -0500

    changed pop method

--- stack.py
+++ stack.py
@@ -9,10 +9,10 @@ class Stack:
         self.storage.push(val)
 
     def pop(self):
-        if self.storage:
+        try:
             return self.storage.pop()
-
-        raise ValueError("The stack is empty.")
+        except:
+            raise ValueError("The stack is empty.")
 
     def __len__(self):
         return self.storage._length
