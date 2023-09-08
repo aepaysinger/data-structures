40a30f815d114bc7c4799b2495687cec65c88c65
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-08 10:33:41 -0500

    queue

---
+++ que_.py
@@ -0,0 +1,19 @@
+class Queue:
+    def __init__(self):
+        pass
+
+    def enqueue(self, val):
+        """adds value to the queue"""
+        pass
+
+    def dequeue(self):
+        """removes the correct item from the queue and returns its value (should raise an error if the queue is empty)"""
+        pass
+
+    def peek(self):
+        """returns the next value in the queue without dequeueing it. If the queue is empty, returns None"""
+        pass
+
+    def size(self):
+        """return the size of the queue. Should return 0 if the queue is empty."""
+        pass 
