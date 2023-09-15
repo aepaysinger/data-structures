7b722896d6b1487c8e0e8554a5c6a1921f1269d5
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-15 15:38:31 -0500

    need to make heap down work

--- binheap.py
+++ binheap.py
@@ -1,25 +1,71 @@
 class BinaryHeap:
     def __init__(self, values=None):
-        self.heap = [values]
+        self.storage = []
         self.size = 0
+
         if values:
             for value in values:
-                heap_up(value)
+                self.size += 1
+                self.storage.append(value)
+                self.heap_up()
                 
 
-        def find_parent(self, index):
-            return (index - 1) // 2
-        
-        def heap_up(self, value):
-            index = self.size
-            self.heap[index] = value
-            parent = find_parent(index)
-            while self.heap[parent] < value:
-                swap(self.heap[parent], index)
-            self.size += 1
-
-        def swap(self, index1, index2):
-            self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
-
-    
             
+
+    def parent_index(self, index):
+        return (index - 1) // 2
+    
+    def left_child_index(self, index):
+        return (2 * index) + 1
+    
+    def right_child_index(self, index):
+        return (2 * index) + 2
+    
+    def has_parent(self, index):
+        return self.parent_index(index) >= 0
+    
+    def has_left_child(self, index):
+        return self.left_child_index(index) < self.size
+    
+    def has_right_child(self, index):
+        return self.left_child_index(index) < self.size
+    
+    def parent_value(self, index):
+        return self.storage[self.parent_index(index)]
+    
+    def left_child_value(self, index):
+        return self.storage[self.left_child_index(index)]
+    
+    def right_child_value(self, index):
+        return self.storage[self.right_child_index(index)]
+    
+    def full_heap(self):
+        return self.size == self.capacity
+    
+    def heap_up(self):
+        index = self.size - 1
+        while (self.has_parent(index)) and self.parent_value(index) > self.storage[index]:
+            self.swap(self.parent_index(index), index)
+
+    def heap_down(self):
+        index = self.size - 1
+        while (self.has_parent(index)) and self.parent_value(index) < self.storage[index]:
+            self.swap(self.parent_index(index), index)
+    
+
+    def swap(self, index1, index2):
+        temp = self.storage[index1]
+        self.storage[index1] = self.storage[index2]
+        self.storage[index2] = temp
+
+    def push(self, value):
+        # if self.full_heap():
+        #     raise ValueError("The heap is full.")
+        self.storage.append(value)
+        self.size += 1
+        self.heap_up()
+
+if __name__ == "__main__":
+    heap = BinaryHeap(5)
+    print(heap.storage)
+
--- test_binheap.py
+++ test_binheap.py
@@ -1,4 +1,39 @@
 from binheap import BinaryHeap
 
+def test_push():
+    binheap = BinaryHeap()
 
-def test
+    binheap.push(4)
+
+    assert binheap.storage == [4]
+    assert binheap.size == 1
+
+    binheap.push(1)
+    binheap.push(3)
+    binheap.push(2)
+
+    assert binheap.storage == [1, 2, 3, 4]
+    assert binheap.parent_index(1) == 0
+    assert binheap.left_child_index(0) == 1
+    assert binheap.right_child_index(0) == 2
+    assert binheap.has_parent(0) == False
+    assert binheap.has_parent(3) == True
+    assert binheap.has_left_child(0) == True
+    assert binheap.has_left_child(2) == False
+    assert binheap.has_right_child(0) == True
+    assert binheap.has_right_child(2) == False
+    assert binheap.parent_value(1) == 1
+    assert binheap.left_child_value(0) == 2
+    assert binheap.right_child_value(0) == 3
+
+
+def test_heap_up():
+    binheap = BinaryHeap([2, 3, 1, 4])
+
+    assert binheap.storage == [1, 3, 2, 4]
+
+def test_heap_down():
+    # binheap = BinaryHeap([2, 3, 1, 4])
+    binheap = BinaryHeap([2, 3, 1])
+    binheap.heap_down()
+    assert binheap.storage == [3, 2, 1]
