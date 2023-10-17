b2f5621c04edb4de16a6e0d063ebd5094e6f2d24
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-10-17 09:44:51 -0500

    weighted graph

---
+++ test_weighted_graphy.py
@@ -0,0 +1,268 @@
+import pytest
+
+from weighted_graph import WeightedGraph
+
+
+def test_add_node():
+    graph = WeightedGraph()
+    graph.add_node(4)
+
+    assert graph._storage == {4: set()}
+    assert graph.nodes() == [4]
+    assert graph.edges() == []
+
+
+def test_add_edge():
+    graph = WeightedGraph()
+    graph.add_edge(4, 6, 21)
+
+    assert graph.nodes() == [4, 6]
+    assert graph.edges() == [(4, 6), (6, 4)]
+    assert graph._storage == {4: {(6, 21)}, 6: {(4, 21)}}
+
+    graph.add_edge(4, 9, 1)
+
+    assert graph.nodes() == [4, 6, 9]
+    assert graph.edges() == [(4, 9), (4, 6), (6, 4), (9, 4)]
+    assert graph._storage == {4: {(6, 21), (9, 1)}, 6: {(4, 21)}, 9: {(4, 1)}}
+
+    graph.add_edge(20, 6, 80)
+
+    assert graph.nodes() == [4, 6, 9, 20]
+    assert graph._storage == {
+        4: {(6, 21), (9, 1)},
+        6: {(4, 21), (20, 80)},
+        9: {(4, 1)},
+        20: {(6, 80)},
+    }
+
+
+def test_del_node():
+    graph = WeightedGraph()
+    graph.add_edge(4, 6, 21)
+    graph.add_edge(4, 9, 1)
+    graph.add_edge(20, 6, 80)
+
+    assert graph._storage == {
+        4: {(6, 21), (9, 1)},
+        6: {(4, 21), (20, 80)},
+        9: {(4, 1)},
+        20: {(6, 80)},
+    }
+
+    graph.del_node(6)
+
+    assert graph._storage == {4: {(9, 1)}, 9: {(4, 1)}, 20: set()}
+    assert graph.nodes() == [4, 9, 20]
+    assert graph.edges() == [(4, 9), (9, 4)]
+    with pytest.raises(ValueError) as exc_info:
+        graph.del_node(5)
+    assert exc_info.value.args[0] == "Node does not exist"
+
+
+def test_del_edge():
+    graph = WeightedGraph()
+    graph.add_edge(6, 9, 12)
+    graph.add_edge(40, 2, 17)
+    graph.add_edge(6, 2, 9)
+    graph.add_node(27)
+
+    assert graph._storage == {
+        6: {(9, 12), (2, 9)},
+        9: {(6, 12)},
+        40: {(2, 17)},
+        2: {(40, 17), (6, 9)},
+        27: set(),
+    }
+
+    graph.del_edge(6, 2)
+
+    assert graph._storage == {
+        6: {(9, 12)},
+        9: {(6, 12)},
+        40: {(2, 17)},
+        2: {(40, 17)},
+        27: set(),
+    }
+    with pytest.raises(ValueError) as exc_info:
+        graph.del_edge(2, 6)
+    assert exc_info.value.args[0] == "Edge does not exist."
+
+
+def test_has_node_true():
+    graph = WeightedGraph()
+    graph.add_edge(6, 9, 67)
+    graph.add_node(3)
+    graph.add_edge(5, 2, 7)
+    graph.add_edge(6, 2, 54)
+    graph.add_node(27)
+
+    assert graph.has_node(6) == True
+    assert graph.has_node(9) == True
+    assert graph.has_node(27) == True
+
+
+def test_has_node_false():
+    graph = WeightedGraph()
+    graph.add_edge(6, 9, 67)
+    graph.add_node(3)
+    graph.add_edge(5, 2, 7)
+    graph.add_edge(6, 2, 54)
+    graph.add_node(27)
+
+    assert graph.has_node(7) == False
+
+
+def test_neighbors():
+    graph = WeightedGraph()
+    graph.add_edge(6, 9, 67)
+    graph.add_node(3)
+    graph.add_edge(5, 2, 7)
+    graph.add_edge(6, 2, 54)
+    graph.add_node(27)
+
+    assert graph._storage == {
+        6: {(9, 67), (2, 54)},
+        9: {(6, 67)},
+        3: set(),
+        5: {(2, 7)},
+        2: {(5, 7), (6, 54)},
+        27: set(),
+    }
+    assert graph.neighbors(2) == [6, 5]
+    with pytest.raises(ValueError) as exc_info:
+        graph.neighbors(8)
+    assert exc_info.value.args[0] == "Value not in graph."
+
+
+def test_adjacent():
+    graph = WeightedGraph()
+    graph.add_edge(4, 6, 21)
+    graph.add_edge(4, 9, 1)
+    graph.add_edge(20, 6, 80)
+
+    assert graph._storage == {
+        4: {(6, 21), (9, 1)},
+        6: {(4, 21), (20, 80)},
+        9: {(4, 1)},
+        20: {(6, 80)},
+    }
+    assert graph.adjacent(6, 4) == True
+    assert graph.adjacent(20, 4) == False
+    assert graph.adjacent(4, 6) == True
+    with pytest.raises(ValueError) as exc_info:
+        graph.adjacent(4, 15)
+    assert exc_info.value.args[0] == "One of the values are not in the graph"
+
+
+def test_depth_first_traversal():
+    graph = WeightedGraph()
+    graph.add_edge(4, 6, 18)
+    graph.add_edge(4, 9, 76)
+    graph.add_edge(4, 12, 13)
+    graph.add_edge(6, 20, 10)
+    graph.add_edge(6, 13, 54)
+    graph.add_edge(13, 7, 24)
+    graph.add_edge(9, 28, 3)
+
+    assert graph._storage == {
+        4: {(6, 18), (9, 76), (12, 13)},
+        6: {(4, 18), (20, 10), (13, 54)},
+        9: {(4, 76), (28, 3)},
+        12: {(4, 13)},
+        20: {(6, 10)},
+        13: {(6, 54), (7, 24)},
+        7: {(13, 24)},
+        28: {(9, 3)},
+    }
+    assert (
+        graph.depth_first_traversal(4)[1] == 6
+        or graph.depth_first_traversal(4)[1] == 9
+        or graph.depth_first_traversal(4)[1] == 12
+    )
+    if graph.depth_first_traversal(4)[1] == 6:
+        assert (
+            graph.depth_first_traversal(4)[2] == 20
+            or graph.depth_first_traversal(4)[2] == 13
+        )
+
+
+def test_breadth_first_traversall_a():
+    graph = WeightedGraph()
+    graph.add_edge(4, 6, 18)
+    graph.add_edge(4, 9, 76)
+    graph.add_edge(4, 12, 13)
+    graph.add_edge(6, 20, 10)
+    graph.add_edge(6, 13, 54)
+    graph.add_edge(13, 7, 24)
+    graph.add_edge(9, 28, 3)
+
+    assert graph._storage == {
+        4: {(6, 18), (9, 76), (12, 13)},
+        6: {(4, 18), (20, 10), (13, 54)},
+        9: {(4, 76), (28, 3)},
+        12: {(4, 13)},
+        20: {(6, 10)},
+        13: {(6, 54), (7, 24)},
+        7: {(13, 24)},
+        28: {(9, 3)},
+    }
+
+    assert (
+        graph.breadth_first_traversal(4)[1] == 6
+        or graph.breadth_first_traversal(4)[1] == 9
+        or graph.breadth_first_traversal(4)[1] == 12
+    )
+    assert (
+        graph.breadth_first_traversal(4)[4] == 20
+        or graph.breadth_first_traversal(4)[4] == 13
+        or graph.breadth_first_traversal(4)[4] == 28
+    )
+    assert graph.breadth_first_traversal(4)[-1] == 7
+
+
+def test_breadth_first_traversall_b():
+    graph = WeightedGraph()
+    graph.add_edge(4, 6, 18)
+    graph.add_edge(4, 9, 76)
+    graph.add_edge(4, 12, 13)
+    graph.add_edge(6, 20, 10)
+    graph.add_edge(6, 13, 54)
+    graph.add_edge(13, 7, 24)
+    graph.add_edge(9, 28, 3)
+
+    assert (
+        graph.breadth_first_traversal(9)[1] == 4
+        or graph.breadth_first_traversal(9)[1] == 28
+    )
+    assert (
+        graph.breadth_first_traversal(9)[3] == 6
+        or graph.breadth_first_traversal(9)[3] == 12
+    )
+    assert graph.breadth_first_traversal(9)[-1] == 7
+
+
+def test_find_paths():
+    graph = WeightedGraph()
+    graph.add_edge("A", "B", 7)
+    graph.add_edge("A", "E", 1)
+    graph.add_edge("B", "C", 3)
+    graph.add_edge("B", "D", 2)
+    graph.add_edge("C", "D", 4)
+    assert len(graph._find_all_paths("A", "D")) == 2
+    assert len(graph._find_all_paths("C", "E")) == 2
+
+
+def test_shortest_path():
+    graph = WeightedGraph()
+    graph.add_edge("A", "B", 7)
+    graph.add_edge("A", "E", 1)
+    graph.add_edge("B", "C", 3)
+    graph.add_edge("B", "D", 2)
+    graph.add_edge("C", "D", 4)
+
+    assert graph._find_shortest_path("A", "D") == ["A", "B", "D"]
+
+    graph.add_edge("D", "E", 6)
+
+    assert graph._find_shortest_path("A", "D") == ["A", "E", "D"]
--- weighted_graph.py
+++ weighted_graph.py
@@ -1,4 +1,4 @@
-class Graph:
+class WeightedGraph:
     def __init__(self):
         self._storage = {}
 
@@ -11,41 +11,50 @@ class Graph:
         edges = []
         for node in self._storage:
             for edge in self._storage[node]:
-                edges.append((node, edge))
+                edges.append((node, edge[0]))
         return edges
 
     def add_node(self, value):
         """Adds a new node(value) to the graph."""
         self._storage[value] = set()
 
-    def add_edge(self, value1, value2):
+    def add_edge(self, value1, value2, weight):
         """
         Add a new edge to the graph connecting the node containing 'value1' and the node containing 'value2'.
         If either value1 or value2 are not already present in the graph, they should be added. If an edge already exisits overwrite it.
         """
         if value1 not in self._storage:
-            self._storage[value1] = {value2}
+            self._storage[value1] = {(value2, weight)}
         if value2 not in self._storage:
-            self._storage[value2] = {value1}
+            self._storage[value2] = {(value1, weight)}
 
-        self._storage[value1].add(value2)
-        self._storage[value2].add(value1)
+        self._storage[value1].add((value2, weight))
+        self._storage[value2].add((value1, weight))
 
     def del_node(self, value):
         """Delete the node containing 'value' from the graph; raise an error if no such node exists."""
+        to_remove = []
         if value in self._storage:
+            for edges in self._storage[value]:
+                to_remove.append((edges[0], (value, edges[1])))
+            for items in to_remove:
+                self._storage[items[0]].remove(items[1])
             del self._storage[value]
-            for edges in self._storage.values():
-                if value in edges:
-                    edges.remove(value)
         else:
             raise ValueError("Node does not exist")
 
     def del_edge(self, value1, value2):
         "Delete the edge connecting 'value1' and 'value2' from the graph; raise an error if no such edge exsits."
-        if value2 in self._storage[value1]:
-            self._storage[value1].remove(value2)
-            self._storage[value2].remove(value1)
+        to_remove = []
+        if (value1, value2) in self.edges():
+            for edges in self._storage[value1]:
+                if edges[0] == value2:
+                    to_remove.append(edges)
+            for edges in self._storage[value2]:
+                if edges[0] == value1:
+                    to_remove.append(edges)
+            self._storage[value1].remove(to_remove[0])
+            self._storage[value2].remove(to_remove[1])
         else:
             raise ValueError("Edge does not exist.")
 
@@ -56,15 +65,20 @@ class Graph:
     def neighbors(self, value):
         """Return a list of all nodes connected to the node containing 'value' by edges; raise an error if 'value' is nto in graph."""
         if value in self._storage:
-            return [edge_value for edge_value in self._storage[value]]
+            return [edge_value[0] for edge_value in self._storage[value]]
         else:
             raise ValueError("Value not in graph.")
 
     def adjacent(self, value1, value2):
         """Return if there is an edge connecting 'value1' and 'value2'; raise an error if either of the supplied values are not in graph."""
+        adjacent = False
         if value1 not in self._storage or value2 not in self._storage:
             raise ValueError("One of the values are not in the graph")
-        return value2 in self._storage[value1]
+        for edges in self._storage[value1]:
+            if edges[0] == value2:
+                adjacent = True
+                break
+        return adjacent
 
     def depth_first_traversal(self, start_val):
         """Return a list of depth_first_traversal starting at start_val"""
@@ -72,13 +86,13 @@ class Graph:
         track = []
 
         for edge in self._storage[start_val]:
-            track.append(edge)
+            track.append(edge[0])
         while track:
             current = track.pop(-1)
             path.append(current)
             for edge in self._storage[current]:
-                if edge not in path:
-                    track.append(edge)
+                if edge[0] not in path:
+                    track.append(edge[0])
         return path
 
     def breadth_first_traversal(self, start_val):
@@ -88,10 +102,10 @@ class Graph:
 
         while need_to_check:
             val = need_to_check.pop(0)
-            for edges in self._storage[val]:
-                if edges not in path:
-                    path.append(edges)
-                    need_to_check.append(edges)
+            for edge, weight in self._storage[val]:
+                if edge not in path:
+                    path.append(edge)
+                    need_to_check.append(edge)
 
         return path
 
@@ -100,21 +114,25 @@ class Graph:
         if start == end:
             return [path]
         paths = []
-        for node in self._storage[start]:
+        for node, weight in self._storage[start]:
             if node not in path:
                 new_paths = self._find_all_paths(node, end, path)
                 for p in new_paths:
                     paths.append(p)
         return paths
-   
-      
 
+    def _find_shortest_path(self, start, end):
+        paths_with_weight = []
 
-if __name__ == "__main__":
-    graph = Graph()
-    graph.add_edge("A", "B")
-    graph.add_edge("A", "E")
-    graph.add_edge("B", "C")
-    graph.add_edge("B", "D")
-    graph.add_edge("C", "D")
-    print(graph._find_all_paths("A", "D"))
+        for path in self._find_all_paths(start, end):
+            total_weight = 0
+
+            for i, node in enumerate(path):
+                if i == len(path) - 1:
+                    break
+                for edges in self._storage[node]:
+                    if edges[0] == path[i + 1]:
+                        total_weight += edges[1]
+            paths_with_weight.append((total_weight, path))
+
+        return min(paths_with_weight)[1]
